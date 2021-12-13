6f736cbf482a52ec1f2a85d93ef2e57100930fd2
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:43:06 -0400

    feature: Add ExtractorDataFromUrl component
    It request the data from passed URL and this passes it to the respective component.

---
+++ src/components/ExtractorDataFromUrl.vue
@@ -0,0 +1,31 @@
+<template>
+    <slot :data='data'></slot>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { doFetch } from '../utils'
+
+export default {
+    props: ['url'],
+    setup(props) {
+        const data = ref(null)
+
+        const options = {
+            url: props.url,
+            data: {},
+            method: 'get'
+        }
+
+        doFetch(
+            options,
+            response => data.value = response.data,
+            err => console.log(err)
+        )
+
+        return {
+            data
+        }
+    } 
+}
+</script>
