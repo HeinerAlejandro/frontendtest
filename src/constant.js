9b0b35a8f2396176f72892c8f370bc54ff6ba413
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:28:13 -0400

    feature: Add projects constant File

---
+++ src/constant.js
@@ -0,0 +1,23 @@
+export const AGE_RANGES = [
+    '18-25',
+    '26-33',
+    '34-40',
+    '40+'
+]
+
+export const GENDERS = [
+    'male',
+    'female'
+]
+
+export const SOCIAL_NETWORKS = [
+    'facebook',
+    'whatsapp',
+    'twitter',
+    'instagram',
+    'tiktok'
+]
+
+const HOST = "http://localhost:8000/"
+export const POLL_URL = `${HOST}polls/`
+export const POLL_SN_PER_AGE_URL = `${POLL_URL}most-used-per-age/`
