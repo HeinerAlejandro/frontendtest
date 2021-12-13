229fc0d5bdec6c9f2a42d5fbec00193c53217d91
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:37:17 -0400

    feature: Add ContainerNumber component

---
+++ src/components/ContainerNumber.vue
@@ -0,0 +1,27 @@
+<template>
+    <a-card :title="title" hoverable>
+        <a-row>
+            <a-col>
+                <h2>{{ content }}</h2>
+            </a-col>
+            <a-col>
+                <slot name="icon" />
+            </a-col>
+        </a-row>
+    </a-card>
+</template>
+
+<script>
+
+export default {
+    props: ['content', 'title'],
+}
+
+</script>
+
+<style>
+    .chart-icon {
+        font-size: 1.8rem;
+        margin-left: .8rem;
+    }
+</style>
