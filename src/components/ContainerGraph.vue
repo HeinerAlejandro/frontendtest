a7732bdc82ec4cd97ddf26d6d21a70d8f5dd4a1c
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:37:36 -0400

    feature: Add ContainerGraph component

---
+++ src/components/ContainerGraph.vue
@@ -0,0 +1,12 @@
+<template>
+    <a-card :title="title" hoverable>
+        <slot></slot>
+    </a-card>
+</template>
+
+<script>
+
+    export default {
+        props: ['title'],
+    }
+</script>
