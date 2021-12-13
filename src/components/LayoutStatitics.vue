089f516df8086fefac6569577c1bd83f931dfc5d
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:39:35 -0400

    feature: Add LayoutStatitics component
    It make a grid for 3 components.

---
+++ src/components/LayoutStatitics.vue
@@ -0,0 +1,30 @@
+<template>
+    <a-row>
+        <a-col span="6">
+            <a-row>
+                <a-col class="mt-col-1">
+                    <slot name="first_panel"></slot>
+                </a-col>
+                <a-col class="mt-col-1">
+                    <slot name="second_panel"></slot>
+                </a-col>
+            </a-row>
+        </a-col>
+        <a-col class="mt-col-1" span="18">
+            <slot name="third_panel"></slot>
+        </a-col>
+    </a-row>
+</template>
+
+<script>
+
+export default {
+    name: 'LayoutStatitics'
+}
+</script>
+
+<style>
+    .mt-col-1 {
+        margin-top: 1rem;
+    }
+</style>
