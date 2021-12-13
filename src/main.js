9bffb252ac22eb725ade57cb246331ad128120ba
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:23:10 -0400

    feature: Add Vue plugins in App.Vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+
+import Antd from 'ant-design-vue'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import VueChartkick from 'vue-chartkick'
+import 'chartkick/chart.js'
+
+import 'ant-design-vue/dist/antd.css'
+import router from './router'
+
+createApp(App)
+    .use(router)
+    .use(Antd)
+    .use(VueChartkick)
+    .mount('#app')
