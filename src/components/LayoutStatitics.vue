31d6e88cc1a2d8820ab604d4ced2c03ec810643f
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 17:35:59 -0400

    feature: Add breakpoints to LayoutStatitics Component

--- src/components/LayoutStatitics.vue
+++ src/components/LayoutStatitics.vue
@@ -1,16 +1,28 @@
 <template>
     <a-row>
-        <a-col span="6">
+        <a-col 
+            :lg="{ span: 6, order: 1 }" 
+            :xs="{ span: 24, order: 2 }">
             <a-row>
-                <a-col class="mt-col-1">
+                <a-col
+                    :xs="{ span: 24 }"
+                    class="mt-col-1"
+                >
                     <slot name="first_panel"></slot>
                 </a-col>
-                <a-col class="mt-col-1">
+                <a-col
+                    :xs="{ span: 24 }"
+                    class="mt-col-1"
+                >
                     <slot name="second_panel"></slot>
                 </a-col>
             </a-row>
         </a-col>
-        <a-col class="mt-col-1" span="18">
+        <a-col 
+            :xs="{ span: 24, order: 1 }"
+            :lg="{ span: 18 }"
+            class="mt-col-1"
+        >
             <slot name="third_panel"></slot>
         </a-col>
     </a-row>
