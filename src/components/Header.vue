08eb2bd8a57682db1a327cda6b47d262fe500bdb
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:30:07 -0400

    feature: Add Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <p style="color:white;display:inline-block;margin-left:24px;font-size:24px; font-weight:bold;">{{ pageTitle }}</p>
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+    props: ['pageTitle']
+}
+</script>
