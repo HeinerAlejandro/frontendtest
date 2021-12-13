f140ae850d9cb633cbf651eb524fa34f63059d8f
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:31:43 -0400

    feature: Add Home Component

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <PollForm />
+</template>
+
+<script>
+
+    import PollForm from '../components/PollForm.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            PollForm,
+        }
+    }
+</script>
